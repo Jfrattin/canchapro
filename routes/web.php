61ded3448343704e293d1a436aa8abad558e82a3
Jfrattin <jfrattin@kozaca.com.ar>
2026-09-20 13:01:07 -0300

    fix: serve frontend HTML views directly and simplify Dockerfile CMD

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index');
+    return response()->file(resource_path('views/index.blade.php'), ['Content-Type' => 'text/html']);
 });
 
 Route::get('/app', function () {
-    return view('app');
+    return response()->file(resource_path('views/app.blade.php'), ['Content-Type' => 'text/html']);
 });
 
 Route::get('/admin', function () {
-    return view('admin');
+    return response()->file(resource_path('views/admin.blade.php'), ['Content-Type' => 'text/html']);
 });
 
 Route::get('/join/{token}', function ($token) {
-    return view('app', ['inviteToken' => $token]);
+    return response()->file(resource_path('views/app.blade.php'), ['Content-Type' => 'text/html']);
 });
